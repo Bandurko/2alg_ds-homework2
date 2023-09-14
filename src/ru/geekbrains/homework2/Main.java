d515d28ff1305830948609e75481964e833d2816
“Sergey” <s22-01-15@yandex.ru>
2023-09-15 02:10:36 +1000

    add HeapSort.java

--- src/ru/geekbrains/homework2/Main.java
+++ src/ru/geekbrains/homework2/Main.java
@@ -1,8 +1,17 @@
+/*
+Реализовать алгоритм пирамидальной сортировки (сортировка кучей).
+* */
 package ru.geekbrains.homework2;
 public class Main {
     public static void main(String[] args) {
 
-
+        int[] array = ArrayUtils.prepareArray();
+        System.out.println("Массив случайных чисел");
+        ArrayUtils.printArray(array);
+        System.out.println();
+        System.out.println("Массив отсортированый методом пирамидальной сортировки (сортировка кучей)");
+        HeapSort.sort(array);
+        ArrayUtils.printArray(array);
 
     }
 }
